ab2a3e200237b4a153d3f26c146e1db7030c3abc
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-26 20:51:35 +0700

    feat: add environment configuration file with API and S3 base URLs.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8765',
+  apiUrl: 'http://localhost:8765/api',
   lmsFileBaseUrl: 'https://intern-hub-lms-service-s3.s3.ap-southeast-1.amazonaws.com',
 };
