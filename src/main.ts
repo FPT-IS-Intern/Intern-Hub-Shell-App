99839d473b9624d2bf4ac43ebb09ad0f60d6b7b1
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-26 20:02:52 +0700

    feat: Initialize module federation and configure global runtime environment.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { initFederation } from '@angular-architects/native-federation';
 
+// Khởi tạo cấu hình Runtime Cấp Toàn Cục (Global Runtime Config)
+const appEnv = {
+  apiUrl: 'http://localhost:8080/api/v1',
+};
+
+(window as any).__env = appEnv;
 try {
   // await initFederation('https://s3.vn-hcm-1.vietnix.cloud/bravos/federation.manifest.json');
   await initFederation('federation.manifest.local.json');
