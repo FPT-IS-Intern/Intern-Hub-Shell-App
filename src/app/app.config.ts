13e4d077b688137674e0d23aded72687ce39e984
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-27 14:19:26 +0700

    feat: Introduce `app.config.ts` to configure Angular application with routing, HTTP client, and design system settings.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpInterceptorFn, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from '@goat-bravos/shared-lib-client';
 
 import { routes } from './app.routes';
@@ -10,7 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor as unknown as HttpInterceptorFn])),
     {
       provide: SYSTEM_DESIGN_CONFIG,
       useValue: {
