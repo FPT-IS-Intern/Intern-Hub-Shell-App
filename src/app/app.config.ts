8d775edbcf0e5ffcd9459eedfcce36f49fa7e1f8
Nhan Huynh Thien <nhanhuynh7115@gmail.com>
2026-02-27 14:19:18 +0700

    update conflict versions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
-import {SYSTEM_DESIGN_CONFIG} from 'dynamic-ds';
+import { SYSTEM_DESIGN_CONFIG } from 'dynamic-ds';
+
+// ng-zorro locale
+import { NZ_I18N, vi_VN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import localeVi from '@angular/common/locales/vi';
+registerLocaleData(localeVi);
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), {
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    { provide: NZ_I18N, useValue: vi_VN },
+    { provide: LOCALE_ID, useValue: 'vi' },
+    {
       provide: SYSTEM_DESIGN_CONFIG,
       useValue: {
         brand: '#E18308',
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,9 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-try {
-  await bootstrapApplication(App, appConfig);
-} catch (err) {
-  console.error(err);
-}
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
 
