a5c2583edde508d771b58de9d1be0cd09becf514
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-03-01 14:59:36 +0700

    feat: Implement core application routing with remote module loading and authentication guards.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { safeLoadRemoteRoutes } from './utils/safe-load-remote';
 import { errorRoutes } from './guards/error.routes';
 import { ShellLayoutComponent } from './layouts/main-layout/shell-layout.component';
 import { Error404LayoutComponent } from './layouts/error-404/error-404.component';
+import { authGuard, createAuthGuard, createNoAuthGuard } from '@goat-bravos/shared-lib-client';
 
 export const routes: Routes = [
   {
@@ -13,10 +14,12 @@ export const routes: Routes = [
   {
     path: 'auth',
     loadChildren: safeLoadRemoteRoutes('auth'),
+    canActivate: [createNoAuthGuard('/homePage')],
   },
   {
     path: '',
     component: ShellLayoutComponent,
+    canActivate: [createAuthGuard('/auth')],
     children: [
       {
         path: 'homePage',
